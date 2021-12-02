8563534e74b20be9f5f2850130cf1a541c59bb1e
hungnguyen1804 <nguyendinhhung1804@gmail.com>
2021-12-02 20:08:19 +0700

    first commit

---
+++ front-react/test-project/src/fetchAPI/url.js
@@ -0,0 +1,5 @@
+const urlBooks = 'http://192.168.1.65:3001/books'
+const urlCategory = 'http://192.168.1.65:3001/category'
+const urlPay = 'http://192.168.1.65:3001/pay'
+
+export {urlBooks , urlCategory, urlPay}
